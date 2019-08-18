552406082a203db0c5bde1ee6e4b0fdeda10aa1e
Qifan Deng <qifand@student.unimelb.edu.au>
2019-08-18 15:57:00 +1000

    finished virtutal db middleware, using SQLite3.

---
+++ src/Server.java
@@ -0,0 +1,11 @@
+import java.util.logging.*;
+
+
+public class Server {
+
+    private final static Logger logger=Logger.getLogger("Server");
+
+    public static void main(String[] args) {
+        logger.info("[*] server is running.");
+    }
+}
