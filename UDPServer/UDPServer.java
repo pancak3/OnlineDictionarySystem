eb99e335718485fa369d42e8a712d4ec57d9c343
Qifan Deng <qifand@student.unimelb.edu.au>
2019-09-03 02:11:41 +1000

    finished handler's logic

--- UDPServer/UDPServer.java
+++ UDPServer/UDPServer.java
@@ -13,15 +13,24 @@ import org.json.simple.parser.ParseException;
 public class UDPServer {
     private final static int UDP_PORT = 7397;
     private final static int QUEUE_SIZE = 42;
+    private final static int HANDLER_POOL_SIZE = 10;
+    private final static int CONFIRMOR_POOL_SIZE = 10;
     private final static BlockingQueue<DatagramPacket> requestQueue = new LinkedBlockingQueue<>(QUEUE_SIZE);
-    private final static BlockingQueue<DatagramPacket> receivedConfirmationQueue = new LinkedBlockingQueue<>(QUEUE_SIZE);
     private final static BlockingQueue<DatagramPacket> respondQueue = new LinkedBlockingQueue<>(QUEUE_SIZE);
+    private final static BlockingQueue<DatagramPacket> requestConfirmationQueue = new LinkedBlockingQueue<>(QUEUE_SIZE);
+    private final static BlockingQueue<DatagramPacket> respondConfirmationQueue = new LinkedBlockingQueue<>(QUEUE_SIZE);
     private final static Database db = new Database();
 
     private final static Logger logger = Logger.getLogger("UDPServer");
 
     public static void main(String[] args) throws IOException {
 
+        for (int i = 0; i < HANDLER_POOL_SIZE; i++) {
+            new Thread(new handler(), "Handler-" + i).start();
+        }
+        for (int i = 0; i < CONFIRMOR_POOL_SIZE; i++) {
+            new Thread(new confirmor(), "Confirmor-" + i).start();
+        }
         try {
 
             DatagramSocket serverSocket = new DatagramSocket(UDP_PORT);
@@ -34,7 +43,7 @@ public class UDPServer {
                 serverSocket.receive(requestPacket);
                 try {
                     requestQueue.put(requestPacket);
-                    receivedConfirmationQueue.put(requestPacket);
+                    requestConfirmationQueue.put(requestPacket);
                     logger.info("Put request and confirmation task in queues");
                 } catch (Exception e) {
                     logger.warning(e.getMessage());
@@ -53,33 +62,33 @@ public class UDPServer {
 
         public void run() {
             try {
+                logger.info(Thread.currentThread().getName() + " is working ...");
                 while (true) {
-                    DatagramPacket requestPacket = requestQueue.take();
-                    String requestContent = new String(requestPacket.getData());
-//                    requestContent = "{\"action\":\"query\",\"data\":{\"wordName\":\"apple\"}}";
-                    logger.info("Handling: " + requestContent);
 
-                    JSONObject requestJson = new JSONObject();
                     try {
-                        requestJson = handleRequest(requestContent);
+                        DatagramPacket requestPacket = requestQueue.take();
+                        String requestContent = new String(requestPacket.getData());
+                        requestContent = "{\"action\":\"query\",\"data\":{\"wordName\":\"apple\"}}";
+                        logger.info("Handling: " + requestContent);
 
+                        //handleRequest() should return respond in Json format
+                        JSONObject respondJson = handleRequest(requestContent);
+                        logger.info("Got legal request: " + respondJson.toJSONString());
+
+                        //Get client attributes from the received data
+                        InetAddress clientAddressUDP = requestPacket.getAddress();
+                        int clientPortUDP = requestPacket.getPort();
+                        String responseContent = requestContent.toUpperCase();
+                        byte[] responseBytes = responseContent.getBytes();
+
+                        //Create a send Datagram packet and send through socket
+                        DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length, clientAddressUDP, clientPortUDP);
+//                  serverSocket.send(responsePacket);
+                        respondQueue.put(responsePacket);
                     } catch (ParseException e) {
                         logger.warning(e.getMessage());
                     }
 
-                    logger.info("Got legal request: " + requestJson.toJSONString());
-
-
-                    //Get client attributes from the received data
-                    InetAddress clientAddressUDP = requestPacket.getAddress();
-                    int clientPortUDP = requestPacket.getPort();
-                    String responseContent = requestContent.toUpperCase();
-                    byte[] responseBytes = responseContent.getBytes();
-
-                    //Create a send Datagram packet and send through socket
-                    DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length, clientAddressUDP, clientPortUDP);
-//                  serverSocket.send(responsePacket);
-                    respondQueue.put(responsePacket);
 
                 }
             } catch (InterruptedException e) {
