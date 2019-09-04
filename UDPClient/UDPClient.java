c71b3f07762190298c3afd4a4fb23e410a89e518
Qifan Deng <qifand@student.unimelb.edu.au>
2019-09-04 15:55:18 +1000

    bug in UDPClient line 99 due to action resquest times, need to solve

--- UDPClient/UDPClient.java
+++ UDPClient/UDPClient.java
@@ -14,6 +14,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import javax.print.DocFlavor;
+import javax.swing.*;
 
 //to remove warning both in IJ and compile, but guess there is better way to use Json
 @SuppressWarnings("unchecked")
@@ -26,7 +27,6 @@ public class UDPClient {
     private final static int MAX_SEND_TIMES = 1;
 
     private final static JSONObject requestJson = (JSONObject) new JSONObject();
-    private final static Action action = (Action) new Action();
 
     private final static Logger logger = Logger.getLogger("UDPClient");
 
@@ -64,111 +64,67 @@ public class UDPClient {
 
         logger.info("UDPClient stated with UDP_SERVER info -> " + UDP_SERVER_ADDR + ":" + UDP_SERVER_PORT);
 
-        // start user
-        new Thread(new User(), "User").start();
-        // start Sender
-        new Thread(new Sender(), "Sender").start();
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+//        String request = userInput.readLine();
 
-        try {
-
-//            System.out.println("This is UDP Client- Enter some text to send to the UDP server");
-            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-
-            // Create a UDP socket object
-            clientSocket = new DatagramSocket();
-            //IP and port for socket (static method)
-
-            // As UDP Datagrams are bounded by fixed message boundaries, define the length
-            byte[] requestBytes;
-            byte[] responseBytes = new byte[1024];
-
-            String request = userInput.readLine();
-            requestBytes = request.getBytes();
-
-            // Create a send Datagram packet and send through socket
-            DatagramPacket sendPacket = new DatagramPacket(requestBytes, requestBytes.length, ServerAddressUDP, serverPortUDP);
-            clientSocket.send(sendPacket);
-
-            // Create a receive Datagram packet and receive through socket
-            DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length);
-            clientSocket.receive(responsePacket);
-            String responseContent = new String(responsePacket.getData());
-            logger.info("Get response from server: " + responseContent);
-            //Close the Socket
-            clientSocket.close();
-
-        } catch (IOException e) {
-            logger.warning(e.getMessage());
-            throw e;
-
-        } finally {
-            if (clientSocket != null)
-                clientSocket.close();
-
-        }
+        SingleClient mainSingleClient = new SingleClient();
+        JSONObject temJson = new JSONObject();
+        JSONObject userInputJson = new JSONObject();
+        temJson.put("wordName", "banana");
+        userInputJson.put("data", temJson);
+        userInputJson.put("action", "query");
+        mainSingleClient.action.updateAction(userInputJson);
+        mainSingleClient.run();
 
     }
 
-    static class User implements Runnable {
-        private final static Logger logger = Logger.getLogger("User");
+
+    private static class SingleClient implements Runnable {
+        private final static String currentThreadName = Thread.currentThread().getName();
+        private final static Logger logger = Logger.getLogger("SingleClient-" + currentThreadName);
+        private final Action action = new Action();
 
         public void run() {
-            logger.info("Starting user ...");
-            JSONObject temJson = new JSONObject();
-            JSONObject userInputJson = new JSONObject();
-            temJson.put("wordName", "banana");
-            userInputJson.put("data", temJson);
-            userInputJson.put("action", "query");
-            action.updateAction(userInputJson);
-
-        }
-    }
-
-    static class Sender implements Runnable {
-        private final static Logger logger = Logger.getLogger("Sender");
-
-        public void run() {
+            logger.info("[*] Running SingleClient-" + currentThreadName);
             try {
+                DatagramSocket singleClientSocket = new DatagramSocket();
+                JSONObject actionJson = this.action.getAction();
+                int actionRequestTimes = Integer.parseInt(actionJson.get("requestTimes").toString());
 
+                while (actionRequestTimes < MAX_SEND_TIMES) {
+                    logger.info("[*] tried " + actionRequestTimes + " times.");
 
-                while (true) {
-                    DatagramSocket responderSocket = new DatagramSocket();
-                    JSONObject actionJson = action.getAction();
-                    int actionRequestTimes = Integer.parseInt(actionJson.get("requestTimes").toString());
+                    actionJson = this.action.getAction();
+                    actionRequestTimes = Integer.parseInt(actionJson.get("requestTimes").toString());
                     long actionTimestamp = Long.parseLong(actionJson.get("timestamp").toString());
-
                     long nowTimestamp = System.currentTimeMillis();
-
                     if (actionJson.containsKey("action") && nowTimestamp - actionTimestamp > REQUEST_SEND_CYCLE_MILLIS) {
-                        if (actionRequestTimes >= MAX_SEND_TIMES) {
-                            logger.warning("Action request sent times exceeded " + MAX_SEND_TIMES + " ,now give it up: " + actionJson.toJSONString());
-                            action.clearAction();
-                        } else {
-                            try {
-                                JSONObject requestJson = (JSONObject) new JSONObject();
 
-                                requestJson.put("action", actionJson.get("action").toString());
-                                requestJson.put("data", actionJson.get("data").toString());
-                                requestJson.put("requestHashCode", actionJson.hashCode());
-                                String requestString = requestJson.toJSONString();
-                                byte[] requestBytes = requestString.getBytes();
-                                DatagramPacket requestPacket = new DatagramPacket(requestBytes, requestBytes.length, InetAddress.getByName(UDP_SERVER_ADDR), UDP_SERVER_PORT);
-                                responderSocket.send(requestPacket);
-                                action.updateAction(actionJson);
+                        try {
+                            JSONObject requestJson = (JSONObject) new JSONObject();
 
-                            } catch (IOException e) {
-                                logger.warning("Error while responding: " + e.getMessage());
-                            }
+                            requestJson.put("action", actionJson.get("action").toString());
+                            requestJson.put("data", actionJson.get("data").toString());
+                            requestJson.put("requestHashCode", actionJson.hashCode());
+                            String requestString = requestJson.toJSONString();
+                            byte[] requestBytes = requestString.getBytes();
+                            DatagramPacket requestPacket = new DatagramPacket(requestBytes, requestBytes.length, InetAddress.getByName(UDP_SERVER_ADDR), UDP_SERVER_PORT);
+                            singleClientSocket.send(requestPacket);
+                            action.updateAction(actionJson);
+
+                        } catch (IOException e) {
+                            logger.warning("Error while responding: " + e.getMessage());
                         }
+
                     }
-                    responderSocket.close();
                 }
+                singleClientSocket.close();
 
             } catch (SocketException e) {
-                logger.warning("Failed to start Sender: " + e.getMessage());
-                System.exit(0);
+                logger.warning("Failed to start SingleClient-" + currentThreadName + ": " + e.getMessage());
             }
         }
+
     }
 
 }
